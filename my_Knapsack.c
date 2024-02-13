6523ab15b25057f832920809a925cfe086ed514a
DanielKuris <danielkuris6@gmail.com>
2024-02-13 20:39:55 +0200

    Fixed dp creation

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -5,25 +5,37 @@
 
 // Function to solve the Boolean Knapsack problem
 int knapSack(int weights[], int values[], int selected_bool[]) {
-    int dp[MAX_ITEMS + 1][MAX_WEIGHT + 1] = {0}; //Dynamic programing table for calculations
+   // Create duplicate arrays with additional element
+    int temp_values[MAX_ITEMS + 1] = {0};
+    int temp_weights[MAX_ITEMS + 1] = {0};
+    for (int i = 1; i <= MAX_ITEMS; i++) {
+        temp_values[i] = values[i - 1];
+        temp_weights[i] = weights[i - 1];
+    }
+
+    // Dynamic programming table for calculations
+    int dp[MAX_ITEMS + 1][MAX_WEIGHT + 1] = {0}; 
+
 
     //Updating the table
     for (int i = 0; i <= MAX_ITEMS; i++) {
         for (int w = 0; w <= MAX_WEIGHT; w++) {
             if (i == 0 || w == 0) {
                 dp[i][w] = 0;
-            } else if (weights[i - 1] <= w) {
-                if (values[i - 1] + dp[i - 1][w - weights[i - 1]] > dp[i - 1][w]) {
+            } else if (weights[i] <= w) {
+                if (values[i] + dp[i - 1][w - weights[i]] > dp[i - 1][w]) {
                     dp[i][w] = values[i - 1] + dp[i - 1][w - weights[i - 1]];
                 } else {
                     dp[i][w] = dp[i - 1][w];
                 }
-            }
+            } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            
         }
     }
     
     //Updating selected_bool
-    int w = MAX_WEIGHT;
     for (int i = MAX_ITEMS; i > 0 && w > 0; i--) {
         if (i - 1 >= 0 && w - weights[i - 1] >= 0 && dp[i][w] != dp[i - 1][w]) {
             selected_bool[i - 1] = 1;
@@ -49,12 +61,6 @@ int main() {
 
     int selected_bool[MAX_ITEMS];
     int max_profit = knapSack(weights, values, selected_bool);
-
-    printf("Selected items (Boolean representation): ");
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        printf("%d ", selected_bool[i]);
-    }
-    printf("\n");
 
     // Create and fill result array
     char result[MAX_ITEMS];
